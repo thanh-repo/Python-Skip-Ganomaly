150b852839c7199e990466c0d03f3cace213de94
Nguyen Thanh Dat <thanhdat6716@gmail.com>
2022-11-22 23:54:09 +0900

    config

--- train.py
+++ train.py
@@ -7,11 +7,13 @@ TRAIN SKIP/GANOMALY
         --dataset cifar10                           \
         --abnormal_class airplane                   \
         --display                                   \
+
+    python train.py --dataset surface_crack --niter 15 --isize 256 --batchsize 8
 """
 
 ##
 # LIBRARIES
-
+import torch
 from options import Options
 from lib.data.dataloader import load_data
 from lib.models import load_model
@@ -22,12 +24,13 @@ from lib.models import load_model
 def main():
     """ Training
     """
+    torch.cuda.empty_cache()
     opt = Options().parse()
-    opt.dataset = "surface_crack"
-    opt.isize = 256
-    opt.device = "cpu"
-    opt.gpu_ids = "-1"
-    opt.model = "skipganomaly"
+    # opt.dataset = "surface_crack"
+    # opt.isize = 256
+    # opt.device = "gpu"
+    # opt.gpu_ids = "0"
+    # opt.model = "skipganomaly"
     data = load_data(opt)
     model = load_model(opt, data)
     model.train()
